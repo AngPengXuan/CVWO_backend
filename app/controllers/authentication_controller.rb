46a62aef453896323df965360d5c8fb8a7f9d32f
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2023-12-26 18:16:16 +0800

    added authentication using jwt

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,51 @@
+# app/controllers/authentication_controller.rb
+class AuthenticationController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      token = encode_token(user_id: user.id, username: user.username)
+      render json: { token: token }
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    # Log out functionality (optional for JWT)
+    # If you have a frontend, clearing the token from client-side is enough
+    render json: { message: 'Logged out successfully' }
+  end
+
+  def validate
+    token = params[:authentication][:token]
+  
+    begin
+      decoded_token = JWT.decode(token, jwt_key, true, algorithm: 'HS256')
+      # puts decoded_token
+  
+      payload = decoded_token.first
+      puts payload
+  
+      # Check for additional conditions if needed
+      # Example: Check for expiration
+      # if payload['exp'] < Time.now.to_i
+      #   return { error: 'Token has expired' }
+      # end
+  
+      # Return the decoded payload if everything is valid
+      payload
+    rescue JWT::DecodeError => e
+      return { error: e.message }
+    end
+  end
+
+  private
+
+  def jwt_key
+    Rails.application.credentials.jwt_key
+
+  def encode_token(payload)
+    JWT.encode(payload, jwt_key, 'HS256') # Replace 'yourSecretKey' with your secret key
+  end
+end
---
+++ test/controllers/authentication_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class AuthenticationControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get authentication_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get authentication_destroy_url
+    assert_response :success
+  end
+end
