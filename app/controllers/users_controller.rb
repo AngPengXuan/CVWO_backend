f4a4af7895142b8abfc8036f8c4a5110f4f0b345
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-01-02 18:32:39 +0800

    fixed sign up, users that sign up will automatically be logged in as well.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,26 @@
 class UsersController < ApplicationController
     def create
       user = User.new(user_params)
-  
+
       if user.save
-        render json: { message: 'User created successfully' }
+        token = encode_token(user_id: user.id)
+        render json: { token: token }
       else
         render json: { error: user.errors.full_messages }, status: :unprocessable_entity
       end
     end
-  
+
     private
-  
+
     def user_params
-      params.permit(:username, :password)
+      params.require(:user).permit(:username, :password)
+    end
+
+    def jwt_key
+      Rails.application.credentials.jwt_key
+    end
+
+    def encode_token(payload)
+      JWT.encode(payload, jwt_key, 'HS256') # Replace 'yourSecretKey' with your secret key
     end
   end
