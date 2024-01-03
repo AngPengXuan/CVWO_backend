b704e581558b84bb945f365f064b2cdb0bc7c8d8
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-01-03 15:08:35 +0800

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ['http://localhost:5173','http://127.0.0.1:5173','https://melodious-choux-dce252.netlify.app/'] # Add your frontend URL
+      origins ['http://localhost:5173','http://127.0.0.1:5173','https://melodious-choux-dce252.netlify.app'] # Add your frontend URL
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
